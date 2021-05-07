30c0a016d53d1f3859a57e1648162ac33f03e1fe
huynino96 <huynino0496@gmail.com>
2021-05-07 22:12:26 +0700

    Add Bot Learn Command

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import os
 
 import discord
 from dotenv import load_dotenv
+from discord.ext import commands
 import json
 
 
@@ -15,6 +16,8 @@ GUILD = os.getenv('DISCORD_GUILD')
 client = discord.Client()
 guild = discord.Guild
 
+bot = commands.Bot(command_prefix='$')
+
 
 @client.event
 async def on_ready():
@@ -23,6 +26,27 @@ async def on_ready():
         f'{client.user} is connected to the following guild:\n'
         f'{guild.name}(id: {guild.id})'
     )
+
+
+def write_json(command):
+    f = open("data.json", "r+")
+    y = json.load(f)
+    string_list = command.split("|")
+    dict.update({string_list[0]: string_list[1]})
+    y.update(dict)
+
+    if os.path.exists("data.json"):
+        os.remove("data.json")
+
+    if "data.json":
+        with open("data.json", "w") as f:
+            json.dump(y, f)
+    f.close()
+
+
+@bot.command(name='learn')
+async def guiding_bot(ctx, command):
+    await ctx.send(command)
 
 
 @client.event
